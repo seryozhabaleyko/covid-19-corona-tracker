b999f8e44bc68f296a506efa04891c5dc37ea3f5
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-06-26 16:04:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
+import Navbar from './components/Navbar';
+
 import './App.module.scss';
+import Countries from './components/Countries';
+import World from './components/World';
+import Footer from './components/Footer';
+import Country from './components/Country';
+
 
 function App() {
 
     return (
-        <div>App</div>
+        <>
+            <header>
+                <Navbar/>
+            </header>
+            <main>
+                <World/>
+                <div className="container">
+                    <Countries/>
+                    <div style={{ height: '100px' }} />
+                </div>
+            </main>
+            <Footer/>
+        </>
     );
 }
 
