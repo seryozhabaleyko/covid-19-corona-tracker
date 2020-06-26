9bce0598f72876a9d680425ab248e95eb4e66ac3
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-06-26 16:05:23 +0300

    Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Navbar.scss';
+
+function Navbar() {
+
+    return (
+        <nav className="navbar">
+            <div className="container d-flex align-items-center justify-content-between">
+                Navbar
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
