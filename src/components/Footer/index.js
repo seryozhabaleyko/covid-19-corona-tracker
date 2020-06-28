aeaf3e70a816a56a54b9548b5c011e464f936769
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-06-28 22:47:39 +0300

    Sticky footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,9 +11,11 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="container">
-                Copyright 2020 &nbsp; <a href="https://github.com/seryozhabaleyko/" target="_blank" style={linkStyles}>Seryozha Baleyko</a> &nbsp; |
+                Copyright 2020 &nbsp; <a href="https://github.com/seryozhabaleyko/" target="_blank"
+                                         rel="noopener noreferrer" style={linkStyles}>Seryozha Baleyko</a> &nbsp; |
                 Sumber data berasal dari
-                API &nbsp; <a href="https://disease.sh/" target="_blank" style={linkStyles}>Novel COVID API</a>
+                API &nbsp; <a href="https://disease.sh/" target="_blank" rel="noopener noreferrer" style={linkStyles}>Novel
+                COVID API</a>
             </div>
         </footer>
     );
