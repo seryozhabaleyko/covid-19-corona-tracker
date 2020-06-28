9c9222511cad37669481e8d57d20060fed9cb771
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-06-28 19:11:05 +0300

    Added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import './Footer.scss';
 
+const linkStyles = {
+    color: '#ccc',
+};
+
 function Footer() {
 
     return (
         <footer className="footer">
             <div className="container">
-                Footer
+                Copyright 2020 &nbsp; <a href="https://github.com/seryozhabaleyko/" target="_blank" style={linkStyles}>Seryozha Baleyko</a> &nbsp; |
+                Sumber data berasal dari
+                API &nbsp; <a href="https://disease.sh/" target="_blank" style={linkStyles}>Novel COVID API</a>
             </div>
         </footer>
     );
