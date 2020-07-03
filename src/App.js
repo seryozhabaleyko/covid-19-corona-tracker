d4c7f470614da4c655002306025e17dc0729e6bd
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-03 14:28:50 +0300

    Switched to hashrouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
