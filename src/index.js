36d829fb113ab5a25fb8f1e3f2ab45657d67991f
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-03 14:19:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.scss';
 ReactDOM.render(
     <React.StrictMode>
         <ErrorBoundary>
-            <App />
+            <App/>
         </ErrorBoundary>
     </React.StrictMode>,
     document.getElementById('root'),
