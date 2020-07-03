6b8f091b1bae9c079429c632ac4de90067bee513
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-03 14:19:24 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,4 +5,9 @@ export const COUNTRIES_URL = '/countries';
 export const DAILY_URL = '/daily';
 export const COUNTRY_DATA_URL = 'https://restcountries.eu/rest/v2/alpha';
 
-export default axios.create({ baseURL: 'https://disease.sh/v2' });
+export const baseUrl = 'https://disease.sh/v3';
+
+export const allUrl = `${baseUrl}/covid-19/all`;
+export const countriesUrl = `${baseUrl}/covid-19/countries`;
+
+export default axios.create({ baseURL: 'https://disease.sh/v3' });
